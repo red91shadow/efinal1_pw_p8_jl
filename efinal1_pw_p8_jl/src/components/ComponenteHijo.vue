8c4e7384ce38684262653c1dc8e6afd3f308c33d
Joel <joshua-lunag@hotmail.com>
2025-06-06 18:44:16 -0500

    Segundo comit de preuba

---
+++ efinal1_pw_p8_jl/src/components/ComponenteHijo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <img :src="imagenes" alt="No se puede mostrar la imagen" />
+    <p>{{nombre}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imagenes: {
+      type: Number,
+      required: true,
+    },
+
+    texto: {
+      type: texto,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      nombre: texto,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ efinal1_pw_p8_jl/src/views/ComponenteHijoview.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+
+    <div class="titulos">
+      <h2>Punta:</h2>
+      <h2>Intento:</h2>
+    </div>
+
+
+    <div class="imagenes">
+      <componente-hijo :imagenes="imagenNegra" />
+      <componente-hijo :imagenes="imagenNegra" />
+      <componente-hijo :imagenes="imagenNegra" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ComponenteHijo from "../components/ComponenteHijo.vue";
+export default {
+  components: { ComponenteHijo },
+  data() {
+    return {
+      
+      image: [
+        {
+          id: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        },
+        {
+          id: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+        },
+        {
+          id: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+        },
+      ],
+      imagenesPokemon: [
+        {
+          id: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        },
+        {
+          id: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+        },
+        {
+          id: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+        },
+      ],
+
+      imagenNegra: "https://placehold.co/200x200/000000/000000.png",
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+.container{
+  margin-top: 6px;
+}
+
+.titulos{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 200px;
+  margin-bottom: 15px;
+}
+
+
+.imagenes{
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  justify-content: center;
+  
+}
+</style>
